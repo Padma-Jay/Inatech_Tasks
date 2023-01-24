02b934ceaa23c86cd46750b503f6673a810d2e06
Padma-Jay <88887627+Padma-Jay@users.noreply.github.com>
2023-01-24 19:33:14 +0530

    Add files via upload

---
+++ Task3/TASK3/ArithOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK3
+{
+    internal class ArithOperations
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+
+
+    }
+    class MulDiv : ArithOperations
+    {
+        public void Mul(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        public void Div(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+        
+    }
+    class Inheritance
+    {
+        public static void Main()
+        {
+            MulDiv obj = new MulDiv();
+            obj.Add(17, 18);
+            obj.Sub(33, 45);
+            obj.Mul(1, 67);
+            obj.Div(100, 10);
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Task3/TASK3/TaxCalculation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class TaxCalculation //Base Class
+    {
+        public void VAT(int amt)
+        {
+            double t1 = 0;
+            t1 = 0.2 * amt;
+            Console.WriteLine("VAT tax is {0}", t1);
+
+        }
+    }
+        class Tax : TaxCalculation //Derived Class
+    {
+        public void GST(int amt)
+        {
+            double t2 = 0;
+            t2 = 0.12 * amt;
+            Console.WriteLine("GST tax is {0}", t2);
+        }
+    }
+         class ImpTax
+    {
+        public static void Main()
+        {
+
+            Tax obj = new Tax();
+            obj.VAT(1000);
+            obj.GST(1000);
+
+        }
+    }
+}
