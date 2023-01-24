9fa6e9d10bb68e7d7b7bd9ae6bcfc016331160a7
Padma-Jay <88887627+Padma-Jay@users.noreply.github.com>
2023-01-24 22:17:41 +0530

    Add files via upload

---
+++ Task6/Task6/Desserts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class Desserts
+    {
+        public static void Main()
+        {
+
+            ArrayList dsrts = new ArrayList();
+            dsrts.Add("Sundae");
+            dsrts.Add("Pudding");
+            dsrts.Add("Gulab Jamun");
+            dsrts.Add("Mousse ");
+            dsrts.Add("Pie");
+
+            //Access the file, read the data and display the items.
+
+            FileStream fs = new FileStream("C:\\Users\\PADMAVATHY\\OneDrive\\Inatech\\DAY5\\Task6\\desserts.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach(object i in dsrts) 
+                sw.WriteLine(i);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+
+            //Display the time of creation of file and size of file.
+
+            FileInfo info = new FileInfo("C:\\Users\\PADMAVATHY\\OneDrive\\Inatech\\DAY5\\Task6\\desserts.txt");
+            Console.WriteLine("Creation Time : {0}", info.CreationTime);
+            Console.WriteLine("Size : {0}", info.Length);
+
+
+
+        }
+    }
+}
---
+++ Task6/Task6/Task6bcs.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class Task6b
+    {
+        public int EmpId { get; set; }
+        public string? EmpName { get; set; }
+        public string? Empgender { get; set; }
+        public int Empage { get; set; }
+
+        public void readfEmp()
+        {
+            FileStream emp = new FileStream("C:\\Users\\PADMAVATHY\\OneDrive\\Inatech\\DAY5\\Task6.Employee.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr = new StreamReader(emp);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadToEnd();
+            Console.WriteLine(str);
+        }
+        public static void Main(string[] args)
+        {
+            Task6b emp = new Task6b();
+            Console.WriteLine("Enter employee name:");
+            emp.EmpName = Console.ReadLine();
+            Console.WriteLine("Enter employee gender:");
+            emp.Empgender = Console.ReadLine();
+            Console.WriteLine("Enter employee ID:");
+            emp.EmpId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter employee age:");
+            emp.Empage = Convert.ToInt32(Console.ReadLine());
+
+            FileStream Emp = new FileStream("C:\\Users\\PADMAVATHY\\OneDrive\\Inatech\\DAY5\\Task6\\Employee.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(Emp);
+            sw.WriteLine(emp.EmpName);
+            
+            sw.WriteLine(emp.Empgender);
+            sw.WriteLine(emp.Empage);
+            sw.WriteLine(emp.EmpId);
+            sw.Flush();
+            sw.Close();
+            Emp.Close();
+            emp.readfEmp();
+        }
+    }
+}
